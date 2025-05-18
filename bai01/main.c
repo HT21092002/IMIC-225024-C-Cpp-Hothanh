b7627efaf88bfb5b3c3847e88bd4557212e4b2b8
Ho Thanh <21092002hothanh@gmail.com>
2025-05-18 19:17:41 +0700

    cập nhật giải phương trình bậc 2 mới nhất

--- bai01/main.c
+++ bai01/main.c
@@ -1,17 +1,21 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <math.h>
 
 int main() {
     float a, b, c;
     float delta, x1, x2;
-    for (int i = 0;; i++) {
-        printf("Nhap a:");
+    char kytu;
+
+    do {
+        printf("Nhap a: ");
         scanf_s("%f", &a);
-        printf("Nhap b:");
+        printf("Nhap b: ");
         scanf_s("%f", &b);
-        printf("Nhap c:");
+        printf("Nhap c: ");
         scanf_s("%f", &c);
+
         if (a == 0) {
+            // Phương trình bậc nhất: bx + c = 0
             if (b == 0) {
                 if (c == 0) {
                     printf("Phuong trinh vo so nghiem.\n");
@@ -26,11 +30,12 @@ int main() {
             }
         }
         else {
+            // Phương trình bậc hai: ax^2 + bx + c = 0
             delta = b * b - 4 * a * c;
 
             if (delta > 0) {
-                x1 = (float)((-b + sqrt(delta)) / (2 * a));
-                x2 = (float)((-b - sqrt(delta)) / (2 * a));
+                x1 = (-b + sqrt(delta)) / (2 * a);
+                x2 = (-b - sqrt(delta)) / (2 * a);
                 printf("Phuong trinh co 2 nghiem phan biet:\n");
                 printf("x1 = %.2f\n", x1);
                 printf("x2 = %.2f\n", x2);
@@ -43,6 +48,12 @@ int main() {
                 printf("Phuong trinh vo nghiem.\n");
             }
         }
-    }
+
+        printf("Nhan Y neu ban muon tiep tuc, nguoc lai nhap ky tu khac: ");
+        getchar(); 
+        scanf_s("%c", &kytu, 1);
+
+    } while (kytu == 'Y' || kytu == 'y');
+
     return 0;
 }
