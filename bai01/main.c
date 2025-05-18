5d8c1d85d9a869ff5c1a56a0061d77ee243d8833
Ho Thanh <21092002hothanh@gmail.com>
2025-05-18 21:30:33 +0700

    cập nhật mới nhất giải phương trình bậc 2

--- bai01/main.c
+++ bai01/main.c
@@ -1,9 +1,42 @@
 ﻿#include <stdio.h>
 #include <math.h>
+void giaiPTbac2(float a, float b, float c) {
+    float delta, x1, x2;
+    if (a == 0) {
+        if (b == 0) {
+            if (c == 0) {
+                printf("Phuong trinh vo so nghiem.\n");
+            }
+            else {
+                printf("Phuong trinh vo nghiem.\n");
+            }
+        }
+        else {
+            x1 = -c / b;
+            printf("Phuong trinh bac nhat, nghiem x = %.2f\n", x1);
+        }
+    }
+    else {
+        delta = b * b - 4 * a * c;
+        if (delta > 0) {
+            x1 = (-b + sqrt(delta)) / (2.0f * a);
+            x2 = (-b - sqrt(delta)) / (2.0f * a);
+            printf("Phuong trinh co 2 nghiem phan biet:\n");
+            printf("x1 = %.2f\n", x1);
+            printf("x2 = %.2f\n", x2);
+        }
+        else if (delta == 0) {
+            x1 = -b / (2.0f * a);
+            printf("Phuong trinh co nghiem kep: x = %.2f\n", x1);
+        }
+        else {
+            printf("Phuong trinh vo nghiem.\n");
+        }
+    }
+}
 
 int main() {
     float a, b, c;
-    float delta, x1, x2;
     char kytu;
 
     do {
@@ -13,9 +46,15 @@ int main() {
         scanf_s("%f", &b);
         printf("Nhap c: ");
         scanf_s("%f", &c);
+        giaiPTbac2(a, b, c);
+        printf("Nhan Y neu ban muon tiep tuc, nguoc lai nhap ky tu khac: ");
+        getchar();
+        scanf_s("%c", &kytu, 1);
 
-        if (a == 0) {
-            // Phương trình bậc nhất: bx + c = 0
+    } while (kytu == 'Y' || kytu == 'y');
+
+    return 0;
+        /*if (a == 0) {
             if (b == 0) {
                 if (c == 0) {
                     printf("Phuong trinh vo so nghiem.\n");
@@ -47,13 +86,5 @@ int main() {
             else {
                 printf("Phuong trinh vo nghiem.\n");
             }
-        }
-
-        printf("Nhan Y neu ban muon tiep tuc, nguoc lai nhap ky tu khac: ");
-        getchar(); 
-        scanf_s("%c", &kytu, 1);
-
-    } while (kytu == 'Y' || kytu == 'y');
-
-    return 0;
+        }*/
 }
