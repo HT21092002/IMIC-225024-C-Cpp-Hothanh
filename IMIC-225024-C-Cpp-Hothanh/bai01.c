c7e4109ee8df9a2fe17f3914e9967810d8b0d0e3
Ho Thanh <21092002hothanh@gmail.com>
2025-05-25 09:58:39 +0700

    cập nhật mới nhất

--- IMIC-225024-C-Cpp-Hothanh/bai01.c
+++ IMIC-225024-C-Cpp-Hothanh/bai01.c
@@ -1,30 +1,31 @@
 ﻿#include <stdio.h>
+void tinh_diem_trung_binh(float diemTB) {
+    if (diemTB >= 8.0) {
+        printf("Hoc sinh xep loai: Gioi\n");
+    }
+    else if (diemTB >= 6.5) {
+        printf("Hoc sinh xep loai: Kha\n");
+    }
+    else if (diemTB >= 5.0) {
+        printf("Hoc sinh xep loai: Trung Binh\n");
+    }
+    else {
+        printf("Hoc sinh xep loai: Yeu\n");
+    }
+}
 
 int main() {
     float diemTB;
     char kytu;
     do {
         // Nhập điểm trung bình từ bàn phím
-        printf("Nhap diem trung binh cua hoc sinh: ");
-        scanf_s("%f", &diemTB);
-
-        // Xếp loại học sinh
-        if (diemTB >= 8.0) {
-            printf("Hoc sinh xep loai: Gioi\n");
-        }
-        else if (diemTB >= 6.5) {
-            printf("Hoc sinh xep loai: Kha\n");
-        }
-        else if (diemTB >= 5.0) {
-            printf("Hoc sinh xep loai: Trung Binh\n");
-        }
-        else {
-            printf("Hoc sinh xep loai: Yeu\n");
-        }
-
+        do {
+            printf("Nhap diem trung binh cua hoc sinh: ");
+            scanf_s("%f", &diemTB);
+        } while (diemTB > 10 || diemTB < 0);
+        tinh_diem_trung_binh(diemTB);
         // Dọn bộ đệm trước khi nhập ký tự
         while ((getchar()) != '\n');  // Xóa kí tự Enter còn sót
-
         printf("Nhan Y neu ban muon tiep tuc, nguoc lai nhap ky tu khac: ");
         scanf_s("%c", &kytu, 1);
 
