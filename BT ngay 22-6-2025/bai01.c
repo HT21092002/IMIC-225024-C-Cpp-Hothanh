f18bd5991a4c0380e71ff64923800740842f9851
Ho Thanh <21092002hothanh@gmail.com>
2025-06-22 09:54:20 +0700

    cập nhật

---
+++ BT ngay 22-6-2025/bai01.c
@@ -0,0 +1,33 @@
+﻿#include <stdio.h>
+#include <stdlib.h>  
+
+int ham_tinh_tong_mang(int a[], int n) {
+    int tong = 0;
+    for (int i = 0; i < n; i++) {  // 
+        tong += a[i];
+    }
+    return tong;
+}
+
+int main() {  // 
+    int n;
+    printf("Nhap vao so luong phan tu trong mang: ");
+    scanf_s("%d", &n);
+
+    int* a = (int*)malloc(n * sizeof(int));
+    if (a == NULL) {
+        printf("Khong du bo nho!\n");
+        return 1;
+    }
+
+    for (int i = 0; i < n; i++) {
+        printf("Nhap a[%d]: ", i);
+        scanf_s("%d", &a[i]);
+    }
+
+    int tong = ham_tinh_tong_mang(a, n);
+    printf("Tong cac so trong mang la: %d\n", tong);
+
+    free(a);  
+    return 0;
+}
