d2b5621d891838a448d1196db664063ff6d3d94a
Ho Thanh <21092002hothanh@gmail.com>
2025-08-10 13:55:35 +0700

    cap nhat

---
+++ C++/hinh_hoc.cpp
@@ -0,0 +1,7 @@
+#include"hinh_hoc.h"
+int hinh_chu_nhat_c::tinh_dien_tich() {
+	return dai * rong;
+}
+int hinh_chu_nhat_c::tinh_chu_vi() {
+	return(dai + rong) * 2;
+}
---
+++ C++/hinh_hoc.h
@@ -0,0 +1,9 @@
+#pragma once
+class hinh_chu_nhat_c 
+{
+public:
+	int dai;
+	int rong;
+	int tinh_dien_tich();
+	int tinh_chu_vi();
+};
