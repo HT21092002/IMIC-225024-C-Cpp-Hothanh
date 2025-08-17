4bd2812e3caf89ccbb23364844146bd1de8f73e2
Ho Thanh <21092002hothanh@gmail.com>
2025-08-17 14:24:27 +0700

    cập nhật

--- C++/bai03.cpp
+++ C++/bai03.cpp
@@ -9,7 +9,7 @@ struct Phan_so
 	int mau;
 };
 
-template <class kieu_du_lieu>
+template <class kieu_du_lieu,int sl>
 class mang
 {
 public:
@@ -23,6 +23,7 @@ public:
 
 	kieu_du_lieu& operator[](int i)
 	{
+		
 		return dia_chi[i];
 	}
 
@@ -31,11 +32,61 @@ public:
 		free(dia_chi);
 	}
 };
+template <class kieu_du_lieu>
+class mang_co_dan
+{
+private:
+    int so_luong;
+    kieu_du_lieu* dia_chi;
 
+public:
+    // Constructor
+    mang_co_dan(int sl)
+    {
+        so_luong = sl;
+        dia_chi = (kieu_du_lieu*)calloc(so_luong, sizeof(kieu_du_lieu));
+    }
+
+    // Destructor
+    ~mang_co_dan()
+    {
+        free(dia_chi);
+    }
+
+    // Toán tử []
+    kieu_du_lieu& operator[](int i)
+    {
+        if (i < 0 || i >= so_luong) {
+            cout << "Chi so khong hop le!\n";
+            exit(1);
+        }
+        return dia_chi[i];
+    }
+
+    // Hàm thay đổi kích thước (resize)
+    void thay_doi_kich_thuoc(int kich_thuoc_moi)
+    {
+        dia_chi = (kieu_du_lieu*)realloc(dia_chi, kich_thuoc_moi * sizeof(kieu_du_lieu));
+        if (!dia_chi) {
+            cout << "Khong du bo nho!\n";
+            exit(1);
+        }
+        // Nếu mảng to hơn thì phần mới sẽ random, có thể memset về 0
+        if (kich_thuoc_moi > so_luong) {
+            for (int i = so_luong; i < kich_thuoc_moi; i++) {
+                dia_chi[i] = kieu_du_lieu(); // gán mặc định
+            }
+        }
+        so_luong = kich_thuoc_moi;
+    }
+
+    // Trả về số lượng
+    int kich_thuoc() const
+    {
+        return so_luong;
+    }
+};
 int main()
 {
-
-
-
 	return 0;
 }
